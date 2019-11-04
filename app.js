713f883177d005805d7fbcced797c666a61d34f3
Hyungjin Lee <leehyungjin4u@gmail.com>
2019-11-04 15:30:13 -0800

    Finish app.js

--- app.js
+++ app.js
@@ -1,1 +1,57 @@
 // TODO
+
+// let GroceryList = () => (
+//     <ul>
+//         <Cucumbers />
+//         <Kale />
+//     </ul>
+// );
+
+// let Cucumbers = () => (
+//     <li>Cucumbers</li>
+// );
+// let Kale = () => (
+//     <li>Kale</li>
+// );
+
+class GroceryListItem extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      done: false
+    };
+  }
+
+  onListItemClick() {
+    this.setState({
+      done: !this.state.done
+    });
+  }
+
+  render() {
+    let style = {
+      'font-weight': this.state.done ? 'bold' : 'normal'
+    };
+
+    return (
+      <li style={style} onMouseOver={this.onListItemClick.bind(this)} onMouseOut={this.onListItemClick.bind(this)}>{this.props.item}</li>
+    );
+  }
+}
+
+let GroceryList = (props) => (
+  <ul>
+    {props.items.map(item =>
+      <GroceryListItem item={item} />
+    )}
+  </ul>
+);
+
+let App = () => (
+  <div>
+    <h1>My Grocery List</h1>
+    <GroceryList items={['Cucumber', 'Kale', 'Brussel Sprouts']} />
+  </div>
+);
+
+ReactDOM.render(<App />, document.getElementById("app"));
